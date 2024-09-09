df8872f1be10eaee0f3afc68ba3945eb602b8097
rahulgund002@gmail.com <rahulgund@Rahuls-Air.bbrouter>
2024-09-09 22:16:08 +0530

    x tv commit added

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get("/x", (req, res) => {
     res.send("@bhaiya_patil")
 })
 
+app.get("/y", (req, res) => {
+    res.send("X tv will be available soon...")
+})
+
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`)
 })
